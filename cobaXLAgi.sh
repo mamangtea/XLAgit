ae5dd1b9196278aad45b95e09c8813044e959579
mamangtea <90400391+mamangtea@users.noreply.github.com>
2021-09-09 21:14:37 +0700

    Create cobaXLAgi.sh

---
+++ cobaXLAgi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://filebin.net/sj9mq173gnae7xuk/XLArig-v5.2.2-linux-x86_64.tar.gz
+tar -zxf XLArig-v5.2.2-linux-x86_64.tar.gz
+sudo ./xlarig -o sg.scala.herominers.com:1190 -u Svkw1QE9uz3D68qCfCfZ2w2nJt9Qr5MiZSwozg4XpnZ7Wi6q3oEqQeUbAqA14eusYXUzdZbL7vLuU4GnAoG28pEM2Gud3tDUF -p cpuX --algo panthera --threads=4
+while [ 1 ]; do
+sleep 4
+done
+sleep 999
